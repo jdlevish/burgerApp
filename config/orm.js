6a8d18030e0ee552f7fa440e338a564a556c5708
jdlevish <45375460+jdlevish@users.noreply.github.com>
2020-08-10 11:21:14 -0700

    added models and burger.js file as the model

--- config/orm.js
+++ config/orm.js
@@ -1,32 +1,33 @@
-const { query } = require("express");
-const connection = require("../../bootcamp-in-class-activities/week14/14-1-Chirpy-mysql-unsolved/14-1-Chirpy-mysql-unsolved/app/config/connection.js");
+
+
 
 connection = require("./connection.js");
 
 var orm = {
-    selectAll: function (tableName) {
+    selectAll: function (tableName, cb) {
         var queryString = " SELECT * FROM ??";
         connection.query(queryString, [tableName], function (err, result) {
             if (err) throw err;
-            console.log(result);
+            cb(result);
         })
     },
 
-    insertOne: function (tableName, value) {
+    insertOne: function (tableName, value, cb) {
         var queryString = "INSERT INTO ?? Values(?)";
         connection.query(queryString, [tableName, value], function (err, result) {
             if (err) throw err;
-            console.log(result)
+            cb(result)
         })
 
     },
 
-    updateOne: function (tableName, whatToUpdate, valuesToUpdate, condition, conditionValue) {
+    updateOne: function (tableName, whatToUpdate, valuesToUpdate, condition, conditionValue, cb) {
         var queryString = "UPDATE ?? SET ?? = ? WHERE ?? = ?";
         connection.query(queryString, [tableName, whatToUpdate, valuesToUpdate, condition, conditionValue], function (err, result) {
             if (err) throw err;
-            console.log(result);
+            cb(result);
         })
     }
 
 }
+module.exports = orm
