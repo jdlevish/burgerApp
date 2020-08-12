bd660b1e8d4e394cef58ff793779ba3991187110
jdlevish <45375460+jdlevish@users.noreply.github.com>
2020-08-11 17:32:18 -0700

    added handlebars views pages and images for views

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
 const express = require("express");
+const handlebars = require("express-handlebars");
+const bodyParser = require("body-parser");
+
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.engine("handlebars", handlebars({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burgers_controllers")
+app.use(routes);
+
+app.use(express.static(__dirname + "/public"));
+
+app.listen(PORT, () => {
+    console.log("listening on port: " + PORT)
+});
